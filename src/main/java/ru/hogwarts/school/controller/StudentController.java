1f2f8895c4ea9cababbbf436459976828193e161
TretyakovaA <putinvivo@bk.ru>
2022-10-09 14:22:24 +0300

    hw 3.2 is done

---
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -0,0 +1,53 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.StudentService;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RestController
+@RequestMapping(path = "/student")
+
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    @GetMapping ("{id}")
+    public ResponseEntity <Student> getStudentInfo (@PathVariable long id){
+        Student student = studentService.findStudent(id);
+        if (student == null){
+            return ResponseEntity.notFound().build();
+        } return ResponseEntity.ok(student);
+    }
+    @PostMapping
+    public  Student createStudent (@RequestBody Student student){
+        return studentService.addStudent(student);
+    }
+    @PutMapping
+    public ResponseEntity <Student> editStudent (@RequestBody Student student, long id){
+        Student foundStudent = studentService.editStudent(id, student);
+        if (foundStudent == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } return ResponseEntity.ok(student);
+    }
+    @DeleteMapping ("{id}")
+    public ResponseEntity <Void> deleteStudent (@PathVariable long id){
+        studentService.deleteStudent(id);
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping
+    public ResponseEntity <Collection<Student>> findByAge (@RequestParam(required = false) int age){
+        if (age>0){
+            return ResponseEntity.ok(studentService.findByAge(age));
+        } return ResponseEntity.ok (Collections.emptyList());
+
+    }
+
+}
